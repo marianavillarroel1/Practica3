1a5b98c4dee99c24e6e5a2196b947c36bf0e4e11
marianavillarroel <marianavillarroel1@upb.edu>
2025-05-12 18:49:11 -0400

    Added patient model

---
+++ Practica3/Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice3.Models
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string CI { get; set; }
+        public string BloodType { get; set; }
+        public string PatientCode { get; set; }
+    }
+}
+
