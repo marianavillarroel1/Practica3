17b21e8e4b73d54c7f1ffbaa73b1fb4927bb162a
marianavillarroel <marianavillarroel1@upb.edu>
2025-05-12 19:06:48 -0400

    P3-001 Added Logs

--- Practica3/Program.cs
+++ Practica3/Program.cs
@@ -1,15 +1,19 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
