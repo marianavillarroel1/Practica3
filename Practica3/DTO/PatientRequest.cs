a5649c7370f943d3692af117e6c158c26a872b14
marianavillarroel <marianavillarroel1@upb.edu>
2025-05-12 18:50:23 -0400

    P3-001 Added patient DTO

---
+++ Practica3/DTO/PatientRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Practica3.DTOs
+{
+    public class PatientRequest
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string CI { get; set; }
+        public string BloodType { get; set; }
+    }
+}
+
